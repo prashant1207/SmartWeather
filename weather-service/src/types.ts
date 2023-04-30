cbda790c12a405d0e62043c7cfdfaef1b76b6ceb
Prashant Tiwari <tiwarip@microsoft.com>
2023-04-30 15:00:38 +0200

    Added types

---
+++ weather-service/src/types.ts
@@ -0,0 +1,69 @@
+export type Result =
+  | {
+      status: "error";
+      message: string;
+    }
+  | {
+      status: "success";
+      data: Weather;
+    };
+
+export type Unit = "metric" | "imperial";
+
+// Generated from https://app.quicktype.io/
+export type Weather = {
+  coord: Coord;
+  weather: WeatherElement[];
+  base: string;
+  main: Main;
+  visibility: number;
+  wind: Wind;
+  clouds: Clouds;
+  dt: number;
+  sys: Sys;
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+};
+
+export type Clouds = {
+  all: number;
+};
+
+export type Coord = {
+  lon: number;
+  lat: number;
+};
+
+export type Main = {
+  temp: number;
+  feels_like: number;
+  temp_min: number;
+  temp_max: number;
+  pressure: number;
+  humidity: number;
+  sea_level: number;
+  grnd_level: number;
+};
+
+export type Sys = {
+  type: number;
+  id: number;
+  country: string;
+  sunrise: number;
+  sunset: number;
+};
+
+export type WeatherElement = {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+};
+
+export type Wind = {
+  speed: number;
+  deg: number;
+  gust: number;
+};
