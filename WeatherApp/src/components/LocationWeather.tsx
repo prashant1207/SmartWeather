d1a4abf86f3922dca1dfadc06b75015adcfe3e9a
Prashant Tiwari <tiwarip@microsoft.com>
2023-04-30 18:42:29 +0200

    Implemented LocationWeather card HomeScreen shows a list

---
+++ WeatherApp/src/components/LocationWeather.tsx
@@ -0,0 +1,117 @@
+import {fetchWeatherByCity} from '@src/query/fetchWeatherByCity';
+import {fetchWeatherByLatLng} from '@src/query/fetchWeatherByLatLng';
+import {getTranslation} from '@src/services/i18n';
+import {Colors} from '@src/styles/Colors';
+import {City} from '@src/types/City';
+import React from 'react';
+import {Text, StyleSheet, TouchableOpacity, View} from 'react-native';
+import {useQuery} from 'react-query';
+import {Weather} from 'weather-service/src/types';
+
+const translations = getTranslation();
+const unit = 'metric';
+const temperatureUnit = unit === 'metric' ? '°C' : '°F';
+
+export function LocationWeather({
+  city,
+}: {
+  city: City;
+  onPress: (weather: Weather, city: City) => void;
+}): React.ReactElement {
+  const resolver =
+    city.type === 'city'
+      ? () => fetchWeatherByCity({cityName: city.name, unit})
+      : () => fetchWeatherByLatLng({location: city.location, unit});
+
+  const {data: result, isLoading} = useQuery(
+    ['city', city.id, 'metric'],
+    resolver,
+  );
+
+  const handler = React.useCallback(() => {}, []);
+  const removeHandler = React.useCallback(() => {}, []);
+
+  if (result && result.status === 'success') {
+    const {data} = result;
+    const weather = data.weather.length > 0 ? data.weather[0] : null;
+    return (
+      <TouchableOpacity style={styles.container} onPress={handler}>
+        <CityText city={city} />
+        <View>
+          <Text style={styles.temperatureText}>
+            {Math.round(data.main.temp)}
+            {temperatureUnit}
+          </Text>
+          {weather && <Text style={styles.subtext}>{weather.main}</Text>}
+        </View>
+      </TouchableOpacity>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <CityText city={city} />
+        <Text>Loading</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <CityText city={city} />
+      <View>
+        <Text>{translations.error}</Text>
+        <TouchableOpacity style={styles.removeButton} onPress={removeHandler}>
+          <Text style={styles.removeText}>{translations.remove}</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+function CityText({city}: {city: City}) {
+  return (
+    <Text style={styles.cityText}>
+      {city.type === 'city' ? city.name : translations.myLocation}
+    </Text>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 16,
+    backgroundColor: Colors.containerBackground,
+  },
+  removeButton: {
+    marginVertical: 4,
+  },
+  removeText: {
+    color: 'red',
+  },
+  cityText: {
+    textTransform: 'capitalize',
+    color: Colors.text,
+    fontWeight: '300',
+  },
+  temperatureText: {
+    textAlign: 'right',
+    color: Colors.text,
+    fontWeight: 'bold',
+  },
+  subtext: {
+    textAlign: 'right',
+    color: Colors.text,
+    fontSize: 12,
+    fontWeight: '300',
+  },
+});
--- WeatherApp/src/screens/HomeScreen.tsx
+++ WeatherApp/src/screens/HomeScreen.tsx
@@ -1,21 +1,40 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {LocationPermission} from '@src/components/LocationPermission';
+import {LocationWeather} from '@src/components/LocationWeather';
+import {useAppLocation} from '@src/context/AppLocation';
+import {City} from '@src/types/City';
 import {StackParamList} from '@src/types/StackParamList';
 import React from 'react';
-import {View, StyleSheet, Button} from 'react-native';
+import {View, StyleSheet, FlatList} from 'react-native';
 
-export function HomeScreen({
-  navigation,
-}: NativeStackScreenProps<StackParamList, 'Home'>): JSX.Element {
+export function HomeScreen({}: NativeStackScreenProps<
+  StackParamList,
+  'Home'
+>): JSX.Element {
+  const [items, setItems] = React.useState<City[]>([]);
+  const {authorized, location} = useAppLocation();
+
+  const renderItem = ({item}: {item: City}) => (
+    <LocationWeather city={item} onPress={() => {}} />
+  );
+
+  React.useEffect(() => {
+    const cityList: City[] = [];
+
+    if (location && typeof location !== 'string') {
+      cityList.push({
+        type: 'location',
+        id: 'location',
+        location: location,
+      });
+    }
+
+    setItems(cityList);
+  }, [authorized, location]);
   return (
     <View style={styles.container}>
-      <LocationPermission authorized={false} />
-      <Button
-        title="Go to Detail"
-        onPress={() => {
-          navigation.navigate('Detail');
-        }}
-      />
+      <LocationPermission authorized={authorized} />
+      <FlatList testID="location-list" data={items} renderItem={renderItem} />
     </View>
   );
 }
